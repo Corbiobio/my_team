5146c151f69f196e6fb941d7a0acb5e9c04c18bb
corbiobio <edarnand@edenschool.fr>
2024-01-09 10:29:16 +0100

    contact page mobile done

--- app.js
+++ app.js
@@ -1,53 +1,59 @@
 let header = document.getElementById("header")
 let footer = document.getElementById("footer")
 
-header.innerHTML = `<h1>myteam</h1>
-
-<div class="menu_burger">
+if (header) {
+  
+  header.innerHTML = `<h1>myteam</h1>
+  
+  <div class="menu_burger">
   
   <label for="checkbox"><img src="/assets/icon-hamburger.svg" alt="menu"></label>
   <input type="checkbox" id="checkbox">
   
   <nav>
-    <label for="checkbox"><img src="/assets/icon-close.svg" alt="cross"></label>
-    
-    <ul>
-      <li><a href="/home">home</a></li>
-      <li><a href="/about">about</a></li>
-      <li><button>contact us</button></li>
-    </ul>
-
-    <img src="/assets/bg-pattern-about-1-mobile-nav-1.svg" class="svg_bottom_right" alt="svg">
-  </nav>
-</div>`
-
-footer.innerHTML += `  <article class="ready">
-<h2>Ready to get started?</h2>
-<button>contact us</button>
-
-<img class="svg_bottom_left" src="/assets/bg-pattern-home-6-about-5.svg" alt="svg">
-</article>
-
-<article class="footer">
-<h2>myteam</h2>
-<nav>
+  <label for="checkbox"><img src="/assets/icon-close.svg" alt="cross"></label>
+  
   <ul>
-    <li><a href="/home">home</a></li>
-    <li><a href="/about">about</a></li>
+  <li><a href="/home">home</a></li>
+  <li><a href="/about">about</a></li>
+  <li><a class="btn" href="/contact">contact us</a></li>
   </ul>
-</nav>
-<p>
+  
+  <img src="/assets/bg-pattern-about-1-mobile-nav-1.svg" class="svg_bottom_right" alt="svg">
+  </nav>
+  </div>`
+}
+ 
+if (footer) {
+  
+  footer.innerHTML += `  <article class="ready">
+  <h2>Ready to get started?</h2>
+  <a href="/contact">contact us</a>
+  
+  <img class="svg_bottom_left" src="/assets/bg-pattern-home-6-about-5.svg" alt="svg">
+  </article>
+  
+  <article class="footer">
+  <h2>myteam</h2>
+  <nav>
+  <ul>
+  <li><a href="/home">home</a></li>
+  <li><a href="/about">about</a></li>
+  </ul>
+  </nav>
+  <p>
   987  Hillcrest Lane <br>
   Irvine, CA <br>
   California 92714 <br>
   Call Us : 949-833-7432 <br>
-</p>
-
-<div>
+  </p>
+  
+  <div>
   <img src="/assets/icon-facebook.svg" alt="facebook">
   <img src="/assets/icon-pinterest.svg" alt="pinterest">
   <img src="/assets/icon-twitter.svg" alt="twitter">
-</div>
-
-<p>Copyright 2020. All Rights Reserved</p>
-</article>`
+  </div>
+  
+  <p>Copyright 2020. All Rights Reserved</p>
+  </article>`
+}
